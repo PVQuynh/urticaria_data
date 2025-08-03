137a97cb35053d8cefdc270e85f7b3a2c7991380
quynhpv <quynhphamvan@tech.admicro.vn>
2025-08-03 20:07:12 +0700

    first commit

---
+++ src/main/java/com/hust/pvq/urticaria_data/UrticariaDataApplication.java
@@ -0,0 +1,13 @@
+package com.hust.pvq.urticaria_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrticariaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrticariaDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hust/pvq/urticaria_data/UrticariaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.pvq.urticaria_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrticariaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
